a9d3ba9b01111cf51a25812f87618d453561522f
fahad eqbal <eqbalfahad@gmail.com>
2021-07-05 01:43:28 +0530

    Placeholder added

--- src/components/Add.jsx
+++ src/components/Add.jsx
@@ -20,7 +20,7 @@ function Add({ addhandler }) {
     }
     return (
         <div className = 'note new'>
-            <textarea className="addnew" cols="10" rows="8" onChange = {changehandler} value ={noteText} />
+            <textarea className="addnew" cols="10" rows="8" onChange = {changehandler} value ={noteText} placeholder = 'Type to add a note..'/>
             <div className="footer">
                 <small>{characterLimit - noteText.length} characters left.</small>
                 <button className="save" onClick = {saveclick}>Save</button>
