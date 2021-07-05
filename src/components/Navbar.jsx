8ac8e4c1b6dfd27cba87e149137e6b45edeed0db
fahad eqbal <eqbalfahad@gmail.com>
2021-07-05 15:16:41 +0530

    Header

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,15 +3,16 @@ import { Link } from 'react-router-dom'
 function Navbar() {
     return (
         <div className = 'navbar-parent'> 
-        <div className="btns">
-        <Link to = "/">
+        <Link to="/">
+            <button className="navbtn header">Local Notes</button>
+            </Link>
+        <Link to = "/"> 
             <button className="navbtn">Home</button>
         </Link>
         <Link to = "/About">
             <button className="navbtn">About dev</button>
             </Link>
             </div>
-        </div>
     )
 }
 
